028a517d73351c3540a97f3c54dc778434868772
Jorge <george16180@live.com>
2023-02-27 18:45:05 -0500

    First commit

---
+++ src/main/java/com/example/videoblogappsecurityjorge/VideoBlogAppSecurityJorgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.videoblogappsecurityjorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoBlogAppSecurityJorgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoBlogAppSecurityJorgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/videoblogappsecurityjorge/VideoBlogAppSecurityJorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videoblogappsecurityjorge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoBlogAppSecurityJorgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
