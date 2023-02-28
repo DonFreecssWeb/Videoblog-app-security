b2a46895739575e7426bfffd03af7c3d64ed9dee
Jorge <george16180@live.com>
2023-02-28 08:07:41 -0500

    created getAllUsers

--- src/main/java/com/example/videoblogappsecurityjorge/VideoBlogAppSecurityJorgeApplication.java
+++ src/main/java/com/example/videoblogappsecurityjorge/VideoBlogAppSecurityJorgeApplication.java
@@ -1,9 +1,12 @@
 package com.example.videoblogappsecurityjorge;
 
+import com.example.videoblogappsecurityjorge.user.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = UserController.class)
 public class VideoBlogAppSecurityJorgeApplication {
 
     public static void main(String[] args) {
